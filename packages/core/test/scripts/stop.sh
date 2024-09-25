26059ed524b0019b455cd5e669f77ab52b8f5f13
Quan Nguyen Ba <quan.nguyenba@pionero.com>
2024-09-25 11:56:44 +0700

    debug port

--- packages/core/test/scripts/stop.sh
+++ packages/core/test/scripts/stop.sh
@@ -3,6 +3,8 @@
 
 lsof -i -P -n | grep LISTEN >> stop.out.txt 2>&1
 
+netstat -ntlp >> stop.out.txt 2>&1
+
 PID=$(lsof -ti :3000)
 echo "Process running on port 3000 with PID $PID"  >> stop.out.txt 2>&1
 
@@ -10,7 +12,7 @@ echo "Process running on port 3000 with PID $PID"  >> stop.out.txt 2>&1
 PID=$(lsof -ti :8000)
 
 if [ -n "$PID" ]; then
-  kill -9 "$PID"
+  kill -9 $PID
   echo "Process running on port 8000 with PID $PID has been killed." >> stop.out.txt 2>&1
 else
   echo "No process is running on port 8000." >> stop.out.txt 2>&1
@@ -20,7 +22,7 @@ fi
 PID=$(lsof -ti :4002)
 
 if [ -n "$PID" ]; then
-  kill -9 "$PID"
+  kill -9 $PID
   echo "Process running on port 4002 with PID $PID has been killed." >> stop.out.txt 2>&1
 else
   echo "No process is running on port 4002." >> stop.out.txt 2>&1
