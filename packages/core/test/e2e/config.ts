e051c43c71589acc950fefca469204e0dad01b46
Quan Nguyen Ba <quan.nguyenba@pionero.com>
2024-09-30 18:13:00 +0700

    change localhost to 0.0.0.0

--- packages/core/test/e2e/config.ts
+++ packages/core/test/e2e/config.ts
@@ -7,7 +7,7 @@ const config = {
   appName: process.env.APP_NAME,
   dynamoEndpoint: process.env.DYNAMODB_ENDPOINT,
   dynamoRegion: process.env.DYNAMODB_REGION,
-  apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3000',
+  apiBaseUrl: process.env.API_BASE_URL || 'http://0.0.0.0:3000',
 }
 
 console.log(config)
--- packages/core/test/scripts/stop.sh
+++ packages/core/test/scripts/stop.sh
@@ -5,28 +5,5 @@ lsof -i -P -n | grep LISTEN >> stop.out.txt 2>&1
 
 netstat -ntlp >> stop.out.txt 2>&1
 
-PID=$(lsof -ti :3000)
-echo "Process running on port 3000 with PID $PID"  >> stop.out.txt 2>&1
-
-# Stop dynamodb local
-PID=$(lsof -ti :8000)
-
-if [ -n "$PID" ]; then
-  kill -9 $PID
-  echo "Process running on port 8000 with PID $PID has been killed." >> stop.out.txt 2>&1
-else
-  echo "No process is running on port 8000." >> stop.out.txt 2>&1
-fi
-
-# Stop 
-PID=$(lsof -ti :4002)
-
-if [ -n "$PID" ]; then
-  kill -9 $PID
-  echo "Process running on port 4002 with PID $PID has been killed." >> stop.out.txt 2>&1
-else
-  echo "No process is running on port 4002." >> stop.out.txt 2>&1
-fi
-
 # Stop docker
 npm run offline:docker:stop >> stop.out.txt 2>&1
