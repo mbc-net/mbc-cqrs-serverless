19fce0bc5e5e6d7cc02294e1b56f4b272532a5ba
Quan Nguyen Ba <quan.nguyenba@pionero.com>
2024-10-17 11:19:38 +0700

    create att bucket when starting sls

--- packages/cli/templates/infra-local/scripts/trigger_ddb_stream.sh
+++ packages/cli/templates/infra-local/scripts/trigger_ddb_stream.sh
@@ -89,4 +89,4 @@ for table in "${tables[@]}"; do
 done
 
 echo "Send a command to trigger command stream tasks"
-aws --endpoint=http://localhost:8000 dynamodb put-item --table-name local-demo-tasks --item "{\"code\":{\"S\":\"test\"},\"updatedBy\":{\"S\":\"92ca4f68-9ac6-4080-9ae2-2f02a86206a4\"},\"createdIp\":{\"S\":\"127.0.0.1\"},\"tenantCode\":{\"S\":\"test\"},\"type\":{\"S\":\"test\"},\"version\":{\"N\":\"0\"},\"createdAt\":{\"S\":\"2024-10-11T13:46:59+07:00\"},\"input\":{\"M\":{}},\"updatedIp\":{\"S\":\"127.0.0.1\"},\"createdBy\":{\"S\":\"92ca4f68-9ac6-4080-9ae2-2f02a86206a4\"},\"requestId\":{\"S\":\"be183dc0-2bc0-4c58-8161-588fb455d44f\"},\"name\":{\"S\":\"test\"},\"sk\":{\"S\":\"${timestamp}\"},\"id\":{\"S\":\"test\"},\"pk\":{\"S\":\"test\"},\"status\":{\"S\":\"QUEUED\"},\"updatedAt\":{\"S\":\"2024-10-11T13:47:00+07:00\"}}"
+aws --endpoint=http://localhost:8000 dynamodb put-item --table-name local-demo-tasks --item "{\"input\":{\"M\":{}},\"sk\":{\"S\":\"${timestamp}\"},\"pk\":{\"S\":\"test\"}}"
