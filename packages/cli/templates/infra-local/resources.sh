09f91f04f7d74cc9fdf718ca07231e126e152bd8
Quan Nguyen Ba <quan.nguyenba@pionero.com>
2024-10-17 11:16:36 +0700

    support running the project in window environment

--- packages/cli/templates/infra-local/resources.sh
+++ packages/cli/templates/infra-local/resources.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+# Load environment variables from .env file
 if [ -f .env ]; then
-  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+  export $(echo $(cat .env | sed 's/#.*//g' | xargs) | envsubst)
 fi
 
-echo "======= create S3 buckets ======="
-aws --endpoint-url=http://localhost:4566 s3 mb s3://$S3_BUCKET_NAME
+echo "======= check if S3 bucket exists ======="
+bucket_exists=$(aws --endpoint-url=http://localhost:4566 s3 ls | grep "$S3_BUCKET_NAME" | wc -l)
+
+if [ "$bucket_exists" -eq 0 ]; then
+  echo "Bucket $S3_BUCKET_NAME does not exist. Creating it..."
+  aws --endpoint-url=http://localhost:4566 s3 mb s3://$S3_BUCKET_NAME
+else
+  echo "Bucket $S3_BUCKET_NAME already exists."
+fi
 
 echo "======= list S3 buckets ======="
 aws --endpoint-url=http://localhost:4566 s3 ls
--- packages/cli/templates/infra-local/scripts/trigger_ddb_stream.sh
+++ packages/cli/templates/infra-local/scripts/trigger_ddb_stream.sh
@@ -39,6 +39,26 @@ for table in "${tables[@]}"; do
 	done
 done
 
+start=$(date +%s)
+while true; do
+	elapsed=$(($(date +%s) - ${start}))
+	if [[ ${elapsed} -gt 10 ]]; then
+		echo "Timeout"
+		exit 1
+	fi
+
+	echo "Check health table tasks"
+	status=$(aws --endpoint=${endpoint} dynamodb describe-table --table-name local-${APP_NAME}-tasks --query 'Table.TableStatus')
+	echo "Table status: ${status}"
+	if [[ "${status}" == "\"ACTIVE\"" ]]; then
+		echo "Table tasks is ACTIVE"
+		break
+	else
+		echo "Table tasks is not ACTIVE"
+		sleep 1
+	fi
+done
+
 # Wait serverless start
 start=$(date +%s)
 while true; do
@@ -67,3 +87,6 @@ for table in "${tables[@]}"; do
 	echo "Send a command to trigger command stream ${table}"
 	aws --endpoint=${endpoint} dynamodb put-item --table-name local-${APP_NAME}-${table}-command --item "{\"pk\": {\"S\": \"test\" }, \"sk\": { \"S\": \"${timestamp}\" }}"
 done
+
+echo "Send a command to trigger command stream tasks"
+aws --endpoint=http://localhost:8000 dynamodb put-item --table-name local-demo-tasks --item "{\"code\":{\"S\":\"test\"},\"updatedBy\":{\"S\":\"92ca4f68-9ac6-4080-9ae2-2f02a86206a4\"},\"createdIp\":{\"S\":\"127.0.0.1\"},\"tenantCode\":{\"S\":\"test\"},\"type\":{\"S\":\"test\"},\"version\":{\"N\":\"0\"},\"createdAt\":{\"S\":\"2024-10-11T13:46:59+07:00\"},\"input\":{\"M\":{}},\"updatedIp\":{\"S\":\"127.0.0.1\"},\"createdBy\":{\"S\":\"92ca4f68-9ac6-4080-9ae2-2f02a86206a4\"},\"requestId\":{\"S\":\"be183dc0-2bc0-4c58-8161-588fb455d44f\"},\"name\":{\"S\":\"test\"},\"sk\":{\"S\":\"${timestamp}\"},\"id\":{\"S\":\"test\"},\"pk\":{\"S\":\"test\"},\"status\":{\"S\":\"QUEUED\"},\"updatedAt\":{\"S\":\"2024-10-11T13:47:00+07:00\"}}"
