02c8ed7f31c6c48e551ba74fe712c1a93c83c430
Quan Nguyen Ba <quan.nguyenba@pionero.com>
2024-11-01 17:47:41 +0700

    test new action for cli

--- packages/cli/src/actions/new.action.spec.ts
+++ packages/cli/src/actions/new.action.spec.ts
@@ -4,11 +4,12 @@ import path from 'path'
 
 import { exportsForTesting } from './new.action'
 
-const { useLatestPackageVersion } = exportsForTesting
+const { usePackageVersion } = exportsForTesting
 
-// create testcase for useLatestPackageVersion function in new.action.ts file
-describe('useLatestPackageVersion', () => {
+// create testcase for usePackageVersion function in new.action.ts file
+describe('usePackageVersion', () => {
   const fname = path.join(__dirname, 'package.json')
+  const packageVersion = '1.0.0'
   const packageJson = JSON.parse(
     readFileSync(path.join(__dirname, '../../package.json')).toString(),
   )
@@ -22,11 +23,11 @@ describe('useLatestPackageVersion', () => {
   })
 
   it('it should update deps', () => {
-    useLatestPackageVersion(__dirname)
+    usePackageVersion(__dirname, packageVersion)
     const tplPackageJson = JSON.parse(readFileSync(fname).toString())
 
-    expect(packageJson.devDependencies['@mbc-cqrs-serverless/core']).toBe(
-      tplPackageJson.dependencies['@mbc-cqrs-serverless/core'],
+    expect(tplPackageJson.dependencies['@mbc-cqrs-serverless/core']).toBe(
+      packageVersion,
     )
     expect(packageJson.version).toBe(
       tplPackageJson.devDependencies['@mbc-cqrs-serverless/cli'],
@@ -36,7 +37,7 @@ describe('useLatestPackageVersion', () => {
   it('it should not update name', () => {
     const { name } = JSON.parse(readFileSync(fname).toString())
 
-    useLatestPackageVersion(__dirname)
+    usePackageVersion(__dirname, packageVersion)
     const tplPackageJson = JSON.parse(readFileSync(fname).toString())
 
     expect(name).toBe(tplPackageJson.name)
@@ -45,7 +46,7 @@ describe('useLatestPackageVersion', () => {
   it('it should not update name with empty name', () => {
     const { name } = JSON.parse(readFileSync(fname).toString())
 
-    useLatestPackageVersion(__dirname, '')
+    usePackageVersion(__dirname, packageVersion, '')
     const tplPackageJson = JSON.parse(readFileSync(fname).toString())
 
     expect(name).toBe(tplPackageJson.name)
@@ -53,7 +54,7 @@ describe('useLatestPackageVersion', () => {
 
   it('it should update name', () => {
     const name = faker.word.sample()
-    useLatestPackageVersion(__dirname, name)
+    usePackageVersion(__dirname, packageVersion, name)
     const tplPackageJson = JSON.parse(readFileSync(fname).toString())
 
     expect(name).toBe(tplPackageJson.name)
